eac9ffe49551d92684a508f01c9f150840839aec
João Pedro Carmo <joaocarmolp@gmail.com>
2024-09-11 15:44:31 -0300

    adiciona cadastrar meta

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-const { select } = require('@inquirer/prompts')
+const { select, input } = require('@inquirer/prompts')
+
+let meta = {
+    value: 'Estudar 1 hora por dia',
+    checked: false,
+}
+
+let metas = [ meta ]
+
+const cadastrarMeta = async () => {
+    const meta = await input({ message: "Digite a meta:"})
+
+    if(meta.length == 0) {
+        console.log('A meta não pode ser vazia.')
+        return
+    }
+    metas.push(
+        { value: meta, checked: false }
+    )
+}
 
 
 const start = async () => {
@@ -25,7 +44,8 @@ const start = async () => {
 
         switch(opcao){
             case "cadastrar": 
-                console.log("Cadastro")
+                await cadastrarMeta()
+                console.log(metas)
                 break
             case "listar":
                 console.log("listar")
